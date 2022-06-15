5b219b1607e0b2cfe6163b8f42c3999ff94e2164
Doreen <wanjirudoreen13@gmail.com>
2022-06-15 22:43:54 +0300

    done assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 const init = () => {
-  
+    const form = document.querySelector('form')
+    form.addEventListener('submit', (e)=>{
+        e.preventDefault();
+        let formInput = document.querySelector('input#searchByID');
+        
+        fetch(`http://localhost:3000/movies/${formInput.value}`)
+        .then(response => response.json())
+        .then(data => {
+            let title = document.querySelector('section#movieDetails h4')
+            let summary = document.querySelector('section#movieDetails p')
+            title.innerText = data.title
+            summary.innerText = data.summary
+        })
+    })
 }
 
 document.addEventListener('DOMContentLoaded', init);
